cd5269e90197ecfb6c1d5f25c3f281cb0d93fb0c
TangYanfei <don1343@foxmail.com>
2020-05-18 14:25:12 +0800

    update:订单详情=>复制单号到剪贴板

--- src/pages/OrderDetail/OrderCard/OrderCard.tsx
+++ src/pages/OrderDetail/OrderCard/OrderCard.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import { View, Text, StyleSheet, PixelRatio } from 'react-native'
+import { View, Text, StyleSheet, PixelRatio, TouchableOpacity, Clipboard } from 'react-native'
 import { Colors } from '../../../constants/Theme'
 import pxToDp from '../../../utils/px2dp'
+import Toast from 'react-native-tiny-toast'
+import AnchorShowcaseManage from '../../AnchorTabs/AnorchMeScreen/AnchorShowcaseManage'
 
-export default function OrderCard(props) {
+export default function OrderCard(props: { detail: any }) {
   const { detail } = props
+
+  const copyOrderSn = () => {
+    Clipboard.setString(detail.orderSn)
+    Toast.show('已复制到剪贴板', { position: 0 })
+  }
 
   return (
     <View style={styles.container}>
@@ -13,7 +20,9 @@ export default function OrderCard(props) {
         justifyContent: 'space-between'
       }}>
         <Text style={styles.text}>订单编号：{detail.orderSn}</Text>
-        <Text style={[styles.text, styles.copyBtn]}>复制</Text>
+        <TouchableOpacity onPress={copyOrderSn}>
+          <Text style={[styles.text, styles.copyBtn]}>复制</Text>
+        </TouchableOpacity>
       </View>
       <Text style={styles.text}>下单时间：{detail.createTime}</Text>
       <Text style={styles.text}>支付方式：{detail.payType === 1 ? '微信支付' : detail.payType === 2 ? '余额支付' : '圈品金支付'}</Text>
