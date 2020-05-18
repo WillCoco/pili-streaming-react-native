1f01b0d7c5c4eaf9e35aeb5e9c0cf68b4731dda9
TangYanfei <don1343@foxmail.com>
2020-05-18 10:18:44 +0800

    update:设置页面新增检查更新

--- src/pages/AboutUs/Form/Form.tsx
+++ src/pages/AboutUs/Form/Form.tsx
@@ -19,7 +19,14 @@ export default function Form() {
 
       <TouchableWithoutFeedback onPress={() => navigation.push('ServiceAgreement')}>
         <View style={styles.formItem}>
-          <Text style={styles.title}>云闪播服务协议</Text>
+          <Text style={styles.title}>云闪播商城服务协议</Text>
+          <Ionicons size={20} name='ios-arrow-forward' color={Colors.darkGrey} />
+        </View>
+      </TouchableWithoutFeedback>
+
+      <TouchableWithoutFeedback onPress={() => {}}>
+        <View style={styles.formItem}>
+          <Text style={styles.title}>检查更新</Text>
           <Ionicons size={20} name='ios-arrow-forward' color={Colors.darkGrey} />
         </View>
       </TouchableWithoutFeedback>
