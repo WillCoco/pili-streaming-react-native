09a7c2130eb36a9a465054c3340f1ea074084cd9
hicks <516469920@qq.com>
2020-06-17 11:16:56 +0800

    feat: clean

--- js/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file Pili SDK fro react-native
- * @author nighca <nighca@live.cn>
- */
-
-import * as consts from './const'
-
-export { consts }
-export { default as Streaming } from './Streaming'
